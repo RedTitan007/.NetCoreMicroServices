a96f60cf71e4b9c34a341bf7af5d91a4087e853a
Sujith Madhyastha <sujith.madhyastha1@outlook.com>
2022-09-25 12:13:05 +0530

    Task #9-Creating Extension Method to Regester Application Services for Ordering MicroServices

---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());//This Will Look for class which is Inheriting from Profile in our case MappingProfile
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());//This Will Look for class which is Inheriting from AbstractValidator
+            services.AddMediatR(Assembly.GetExecutingAssembly());//This Will Look for class which is Inheriting from IRequestHandler    
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
