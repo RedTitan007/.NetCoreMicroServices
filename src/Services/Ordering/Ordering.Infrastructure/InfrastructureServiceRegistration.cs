8bfd9ef2ae4d9dfdd152c5f60b4a51064966aaaf
Sujith Madhyastha <sujith.madhyastha1@outlook.com>
2022-10-03 19:20:39 +0530

    Task #9-Creating Infrastructure Registering Services Method to use in StartUp file of Ordering.API

---
+++ src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Mail;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            //Her Typeof is required as Type Convention which is required for 
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+            ///Check 9:30 of video 27 about LIfecycle
+
+            return services;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }//Constructor parameter to get the context injection 14:05 in video number 26
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                                .Where(o => o.UserName == userName)
+                                .ToListAsync();
+            return orderList;
+        }
+    }
+}
